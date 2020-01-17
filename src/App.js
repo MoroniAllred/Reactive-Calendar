aba6180ac3cd09adcc1ff5f1c78839d33b6f8596
Moroni Allred <moroniallred@gmail.com>
2020-01-16 17:02:46 -0700

    date range

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Calendar from 'react-calendar';
+ 
+class App extends Component {
+  state = {
+    date: new Date(),
+    // dateRange: [],
+    // maxDate: "",
+    selectRang: false
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  onChange = date => this.setState({ date,
+   maxDate: date })
+ 
+  startSelct = () => this.setState({selectRang: !this.state.selectRang})
+  // componentDidMount(){
+  //   this.setState({
+  //     date: this.state.date,
+  //     maxDate: this.state.date
+  //   })
+  //   console.log(this.state.maxDate)
+  // }
+  
+
+
+  // onClickDay = dateRange => this.setState({dateRange})
+ 
+  // selectRange = dateRange => this.setState({dateRange})
+  render(props) {
+    return (
+      <div>
+        <Calendar
+          onChange={this.onChange}
+          value={this.state.date}
+          // onClickDay={this.onClickDay}
+          selectRange={this.state.selectRang}
+          // maxDate={this.maxDate}
+        />
+        {/* {console.log(this.state.date)} */}
+        {/* {console.log(this.state.dateRange)} */}
+        {console.log(this.state.date)}
+        <button onClick={this.startSelct}></button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App
